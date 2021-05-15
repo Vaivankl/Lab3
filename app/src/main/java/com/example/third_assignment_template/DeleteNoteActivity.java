5a80a78be068bfa4292ddf623424c19dfe4f201a
Vaivankl <vaiva.niuklyte@knf.stud.vu.lt>
2021-05-15 11:10:52 +0300

    Delete layout

---
+++ app/src/main/java/com/example/third_assignment_template/DeleteNoteActivity.java
@@ -0,0 +1,14 @@
+package com.example.third_assignment_template;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DeleteNoteActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_note)
+
+}
